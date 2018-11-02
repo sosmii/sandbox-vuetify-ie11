0bf4441032fefdd0cfd9ab0be74cb11d02e5f5e6
sosmii <setsuya.osumi.private@gmail.com>
2018-11-02 10:18:01 +0900

    to pug

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,23 @@
-<template>
-  <v-layout row>
-    <v-flex 
-      xs12 
-      sm6 
-      offset-sm3>
-      <v-card>
-        <v-toolbar 
-          color="teal" 
-          dark>
-          <v-toolbar-side-icon/>
-
-          <v-toolbar-title>Topics</v-toolbar-title>
-
-          <v-spacer/>
-
-          <v-btn icon>
-            <v-icon>more_vert</v-icon>
-          </v-btn>
-        </v-toolbar>
-
-        <v-list>
-          <v-list-group
-            v-for="item in items"
-            v-model="item.active"
-            :key="item.title"
-            :prepend-icon="item.action"
-            no-action
-          >
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-
-            <v-list-tile
-              v-for="subItem in item.items"
-              :key="subItem.title"
-            >
-              <v-list-tile-content>
-                <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
-              </v-list-tile-content>
-
-              <v-list-tile-action>
-                <v-icon>{{ subItem.action }}</v-icon>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list-group>
-        </v-list>
-      </v-card>
-    </v-flex>
-  </v-layout>
+<template lang="pug">
+v-layout(row='')
+  v-flex(xs12='', sm6='', offset-sm3='')
+    v-card
+      v-toolbar(color='teal', dark='')
+        v-toolbar-side-icon
+          v-toolbar-title Topics
+          v-spacer
+            v-btn(icon='')
+              v-icon more_vert
+      v-list
+        v-list-group(v-for='item in items', v-model='item.active', :key='item.title', :prepend-icon='item.action', no-action='')
+          v-list-tile(slot='activator')
+            v-list-tile-content
+              v-list-tile-title {{ item.title }}
+          v-list-tile(v-for='subItem in item.items', :key='subItem.title')
+            v-list-tile-content
+              v-list-tile-title {{ subItem.title }}
+            v-list-tile-action
+              v-icon {{ subItem.action }}
 </template>
 
 <script>
